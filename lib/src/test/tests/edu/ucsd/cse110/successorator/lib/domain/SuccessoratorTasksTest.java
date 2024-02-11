2855d204506fb5063ff754172c94ad5fa0740e0a
yeeeee1015 <eric.ye04@yahoo.com>
2024-02-10 22:14:53 -0800

    Implemented US-5 Unit Test

---
+++ lib/src/test/tests/edu/ucsd/cse110/successorator/lib/domain/SuccessoratorTasksTest.java
@@ -0,0 +1,24 @@
+package edu.ucsd.cse110.successorator.lib.domain;
+
+import static org.junit.Assert.assertEquals;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SuccessoratorTasksTest extends TestCase {
+    @Test
+    public void testToggleComplete() {
+        List<SuccessoratorTask> tasks = new ArrayList<>();
+        tasks.add(new SuccessoratorTask(1, "Task 1", 0, false));
+        tasks.add(new SuccessoratorTask(2, "Task 2", 1, false));
+        tasks.add(new SuccessoratorTask(3, "Task 3", 2, false));
+
+        List<SuccessoratorTask> modifiedTasks = SuccessoratorTasks.toggleComplete(tasks, 1);
+
+        assertEquals(true, modifiedTasks.get(1).getIsComplete().booleanValue());
+    }
+}
