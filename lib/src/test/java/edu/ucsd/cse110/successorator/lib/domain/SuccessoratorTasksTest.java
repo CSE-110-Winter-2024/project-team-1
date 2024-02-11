5bfb7ef09b4f3baa6dbf2a95ec56ba8e2a55bde9
jensenmckenzie <jensen.mckenzie@outlook.com>
2024-02-11 14:11:00 -0800

    Revert "Reverted commits not related to US-5"
    This reverts commit ec66d16f776aa8f2fad8d8bd9288af8e700dc767.

--- lib/src/test/java/edu/ucsd/cse110/successorator/lib/domain/SuccessoratorTasksTest.java
+++ lib/src/test/java/edu/ucsd/cse110/successorator/lib/domain/SuccessoratorTasksTest.java
@@ -10,5 +10,74 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SuccessoratorTasksTest {
-    // todo: implement tests for SuccessoratorTasks
+    @Test
+    public void testToggleCompleteStart() {
+        List<SuccessoratorTask> tasks = new ArrayList<>();
+        tasks.add(new SuccessoratorTask(1, "Task 1", 0, false));
+        tasks.add(new SuccessoratorTask(2, "Task 2", 1, false));
+        tasks.add(new SuccessoratorTask(3, "Task 3", 2, false));
+
+        List<SuccessoratorTask> modifiedTasks = SuccessoratorTasks.toggleComplete(tasks, 0);
+
+        assertEquals(true, modifiedTasks.get(0).getIsComplete().booleanValue());
+        assertEquals(false, modifiedTasks.get(1).getIsComplete().booleanValue());
+        assertEquals(false, modifiedTasks.get(2).getIsComplete().booleanValue());
+    }
+
+    @Test
+    public void testToggleCompleteMiddle() {
+        List<SuccessoratorTask> tasks = new ArrayList<>();
+        tasks.add(new SuccessoratorTask(1, "Task 1", 0, false));
+        tasks.add(new SuccessoratorTask(2, "Task 2", 1, false));
+        tasks.add(new SuccessoratorTask(3, "Task 3", 2, false));
+
+        List<SuccessoratorTask> modifiedTasks = SuccessoratorTasks.toggleComplete(tasks, 1);
+
+        assertEquals(false, modifiedTasks.get(0).getIsComplete().booleanValue());
+        assertEquals(true, modifiedTasks.get(1).getIsComplete().booleanValue());
+        assertEquals(false, modifiedTasks.get(2).getIsComplete().booleanValue());
+    }
+
+    @Test
+    public void testToggleCompleteEnd() {
+        List<SuccessoratorTask> tasks = new ArrayList<>();
+        tasks.add(new SuccessoratorTask(1, "Task 1", 0, false));
+        tasks.add(new SuccessoratorTask(2, "Task 2", 1, false));
+        tasks.add(new SuccessoratorTask(3, "Task 3", 2, false));
+
+        List<SuccessoratorTask> modifiedTasks = SuccessoratorTasks.toggleComplete(tasks, 2);
+
+        assertEquals(false, modifiedTasks.get(0).getIsComplete().booleanValue());
+        assertEquals(false, modifiedTasks.get(1).getIsComplete().booleanValue());
+        assertEquals(true, modifiedTasks.get(2).getIsComplete().booleanValue());
+    }
+
+    @Test
+    public void testToggleCompleteEmpty() {
+        // catch the illegal argument exception
+        List<SuccessoratorTask> tasks = new ArrayList<>();
+
+        try {
+            SuccessoratorTasks.toggleComplete(tasks, 0);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid sortOrder", e.getMessage());
+
+        }
+    }
+
+    @Test
+    public void testToggleCompleteInvalidIndex() {
+        // catch the illegal argument exception
+        List<SuccessoratorTask> tasks = new ArrayList<>();
+        tasks.add(new SuccessoratorTask(1, "Task 1", 0, false));
+        tasks.add(new SuccessoratorTask(2, "Task 2", 1, false));
+        tasks.add(new SuccessoratorTask(3, "Task 3", 2, false));
+
+        try {
+            SuccessoratorTasks.toggleComplete(tasks, 3);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid sortOrder", e.getMessage());
+        }
+    }
+
 }
