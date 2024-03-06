de1dbcd7bd59170fae385a0a9ee5fda06e7377dd
literally1984bygeorgeorwell <brjin@ucsd.edu>
2024-03-05 19:12:11 -0800

    70: add interval field to SuccessoratorTask git wont let me start a commit with a #, so we work with what we have. This field is necessary for eventual implementation of a recurring task function because we need to be able to know how often to reschedule tasks.

--- lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/SuccessoratorTask.java
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/SuccessoratorTask.java
@@ -16,15 +16,17 @@ public class SuccessoratorTask {
 
     private final @NonNull long dueDate;
 
+    private final @Nullable TaskInterval interval;
 
 
-    public SuccessoratorTask(@Nullable Integer id, @NonNull String name, int sortOrder, @NonNull Boolean isComplete, @NonNull TaskType type, @NonNull long dueDate) {
+    public SuccessoratorTask(@Nullable Integer id, @NonNull String name, int sortOrder, @NonNull Boolean isComplete, @NonNull TaskType type, @NonNull long dueDate, TaskInterval interval) {
         this.name = name;
         this.sortOrder = sortOrder;
         this.id = id;
         this.isComplete = isComplete;
         this.type = type;
         this.dueDate = dueDate;
+        this.interval = interval;
     }
 
     public @Nullable Integer getId() {
@@ -51,23 +53,31 @@ public class SuccessoratorTask {
         return dueDate;
     }
 
+    public @Nullable TaskInterval getInterval() {
+        return interval;
+    }
+
     public SuccessoratorTask withId(int id) {
-        return new SuccessoratorTask(id, name, sortOrder, isComplete, type, dueDate);
+        return new SuccessoratorTask(id, name, sortOrder, isComplete, type, dueDate, interval);
     }
 
     public SuccessoratorTask withSortOrder(int sortOrder) {
-        return new SuccessoratorTask(id, name, sortOrder, isComplete, type, dueDate);
+        return new SuccessoratorTask(id, name, sortOrder, isComplete, type, dueDate, interval);
     }
 
     public SuccessoratorTask withIsComplete(Boolean isComplete) {
-        return new SuccessoratorTask(id, name, sortOrder, isComplete, type, dueDate);
+        return new SuccessoratorTask(id, name, sortOrder, isComplete, type, dueDate, interval);
     }
 
     public SuccessoratorTask withType(TaskType type) {
-        return new SuccessoratorTask(id, name, sortOrder, isComplete, type, dueDate);
+        return new SuccessoratorTask(id, name, sortOrder, isComplete, type, dueDate, interval);
     }
 
     public SuccessoratorTask withDueDate(long dueDate) {
-        return new SuccessoratorTask(id, name, sortOrder, isComplete, type, dueDate);
+        return new SuccessoratorTask(id, name, sortOrder, isComplete, type, dueDate, interval);
+    }
+
+    public SuccessoratorTask withInterval(TaskInterval interval) {
+        return new SuccessoratorTask(id, name, sortOrder, isComplete, type, dueDate, interval);
     }
 }
