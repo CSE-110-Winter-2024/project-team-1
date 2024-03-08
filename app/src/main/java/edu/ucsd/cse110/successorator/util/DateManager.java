83b113bb2365c5fd8612fe448a7f4498128ba29f
fagao8 <fagao@ucsd.edu>
2024-03-07 21:01:01 -0800

    feat: display for recurrence options

--- app/src/main/java/edu/ucsd/cse110/successorator/util/DateManager.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/util/DateManager.java
@@ -3,10 +3,12 @@ package edu.ucsd.cse110.successorator.util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class DateManager {
     private static final String DATE_FORMAT = "EEEE M/d";
+    private static final String LONG_DATE_FORMAT = "MMMM d, yyyy";
 
     private Calendar calendar = Calendar.getInstance();
 
@@ -14,9 +16,33 @@ public class DateManager {
         calendar.add(Calendar.HOUR, -2);
     }
 
+    // mocking
+    public DateManager(Calendar calendar) {
+        this.calendar = calendar;
+    }
+
     public String getDate() {
-        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
+        return getFormattedDate(DATE_FORMAT);
+    }
+
+    public String getLongDate() {
+        return getFormattedDate(LONG_DATE_FORMAT);
+    }
+
+    public String getFormattedDate(String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.getDefault());
         return sdf.format(calendar.getTime());
+    }
+
+    public long getDateFromFormattedString(String dateString) {
+        SimpleDateFormat sdf = new SimpleDateFormat(LONG_DATE_FORMAT, Locale.getDefault());
+        try {
+            Date date = sdf.parse(dateString);
+            return date.getTime();
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public String incrementDate() {
