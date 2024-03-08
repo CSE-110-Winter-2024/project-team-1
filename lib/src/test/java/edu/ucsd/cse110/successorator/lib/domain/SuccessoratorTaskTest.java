e8b6ae9de59e0d85f264f3f1be6038fe19877d59
literally1984bygeorgeorwell <brjin@ucsd.edu>
2024-03-07 19:26:18 -0800

    73: add tests for new rescheduling methods Unit tests for rescheduleTask() and rescheduleTasks()

--- lib/src/test/java/edu/ucsd/cse110/successorator/lib/domain/SuccessoratorTaskTest.java
+++ lib/src/test/java/edu/ucsd/cse110/successorator/lib/domain/SuccessoratorTaskTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.util.Calendar;
+
 public class SuccessoratorTaskTest {
 
     @Test
@@ -32,6 +35,10 @@ public class SuccessoratorTaskTest {
         assertEquals(task.getName(), updatedTask.getName());
         assertEquals(task.getSortOrder(), updatedTask.getSortOrder());
         assertEquals(task.getIsComplete(), updatedTask.getIsComplete());
+        assertEquals(task.getType(), updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
+        assertEquals(task.getDueDate(), updatedTask.getDueDate());
+        assertEquals(task.getInterval(), updatedTask.getInterval());
     }
 
     @Test
@@ -45,6 +52,10 @@ public class SuccessoratorTaskTest {
         assertEquals(task.getName(), updatedTask.getName());
         assertEquals(sortOrder, updatedTask.getSortOrder());
         assertEquals(task.getIsComplete(), updatedTask.getIsComplete());
+        assertEquals(task.getType(), updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
+        assertEquals(task.getDueDate(), updatedTask.getDueDate());
+        assertEquals(task.getInterval(), updatedTask.getInterval());
     }
 
     @Test
@@ -58,6 +69,10 @@ public class SuccessoratorTaskTest {
         assertEquals(task.getName(), updatedTask.getName());
         assertEquals(task.getSortOrder(), updatedTask.getSortOrder());
         assertEquals(isComplete, updatedTask.getIsComplete());
+        assertEquals(task.getType(), updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
+        assertEquals(task.getDueDate(), updatedTask.getDueDate());
+        assertEquals(task.getInterval(), updatedTask.getInterval());
     }
 
     @Test
@@ -72,6 +87,9 @@ public class SuccessoratorTaskTest {
         assertEquals(task.getSortOrder(), updatedTask.getSortOrder());
         assertEquals(task.getIsComplete(), updatedTask.getIsComplete());
         assertEquals(taskType, updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
+        assertEquals(task.getDueDate(), updatedTask.getDueDate());
+        assertEquals(task.getInterval(), updatedTask.getInterval());
     }
 
     @Test
@@ -86,6 +104,124 @@ public class SuccessoratorTaskTest {
         assertEquals(task.getSortOrder(), updatedTask.getSortOrder());
         assertEquals(task.getIsComplete(), updatedTask.getIsComplete());
         assertEquals(task.getType(), updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
         assertEquals(dueDate, updatedTask.getDueDate());
+    }
+
+    @Test
+    public void testRescheduleDaily() {
+        SuccessoratorTask task = new SuccessoratorTask(null, "Daily Task", 0, false, TaskType.Recurring, LocalDate.now().toEpochDay(), LocalDate.now().toEpochDay(), TaskInterval.Daily);
+        SuccessoratorTask updatedTask = SuccessoratorTasks.rescheduleTask(task);
+        assertEquals(task.getName(), updatedTask.getName());
+        assertEquals(task.getSortOrder(), updatedTask.getSortOrder());
+        assertEquals(task.getIsComplete(), updatedTask.getIsComplete());
+        assertEquals(task.getType(), updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
+        assertEquals(task.getDueDate() + 1, updatedTask.getDueDate());
+        assertEquals(task.getInterval(), updatedTask.getInterval());
+    }
+
+    @Test
+    public void testRescheduleWeekly() {
+        SuccessoratorTask task = new SuccessoratorTask(null, "Weekly Task", 0, false, TaskType.Recurring, LocalDate.now().toEpochDay(), LocalDate.now().toEpochDay(), TaskInterval.Weekly);
+        SuccessoratorTask updatedTask = SuccessoratorTasks.rescheduleTask(task);
+        assertEquals(task.getName(), updatedTask.getName());
+        assertEquals(task.getSortOrder(), updatedTask.getSortOrder());
+        assertEquals(task.getIsComplete(), updatedTask.getIsComplete());
+        assertEquals(task.getType(), updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
+        assertEquals(task.getDueDate() + 6, updatedTask.getDueDate());
+        assertEquals(task.getInterval(), updatedTask.getInterval());
+    }
+
+    @Test
+    public void testRescheduleMonthlyHappy() {
+        SuccessoratorTask task = new SuccessoratorTask(null, "Monthly Task", 0, false, TaskType.Recurring, 19790, 19790, TaskInterval.Monthly);
+        SuccessoratorTask updatedTask = SuccessoratorTasks.rescheduleTask(task);
+
+        assertEquals(task.getName(), updatedTask.getName());
+        assertEquals(task.getSortOrder(), updatedTask.getSortOrder());
+        assertEquals(task.getIsComplete(), updatedTask.getIsComplete());
+        assertEquals(task.getType(), updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
+        assertEquals(19817, updatedTask.getDueDate());
+        assertEquals(task.getInterval(), updatedTask.getInterval());
+    }
+
+    @Test
+    public void testRescheduleYearlyHappy() {
+        SuccessoratorTask task = new SuccessoratorTask(null, "Yearly Task", 0, false, TaskType.Recurring, 19790, 19790, TaskInterval.Yearly);
+        SuccessoratorTask updatedTask = SuccessoratorTasks.rescheduleTask(task);
+
+        assertEquals(task.getName(), updatedTask.getName());
+        assertEquals(task.getSortOrder(), updatedTask.getSortOrder());
+        assertEquals(task.getIsComplete(), updatedTask.getIsComplete());
+        assertEquals(task.getType(), updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
+        assertEquals(20155, updatedTask.getDueDate());
+        assertEquals(task.getInterval(), updatedTask.getInterval());
+    }
+
+    @Test
+    public void testRescheduleFifthDay() {
+        long fifthDayTest = 19812; // March 30th, 2024 (5th Saturday)
+        SuccessoratorTask task = new SuccessoratorTask(null, "Fifth Saturday Task", 0, false, TaskType.Recurring, fifthDayTest, fifthDayTest, TaskInterval.Monthly);
+        SuccessoratorTask updatedTask = SuccessoratorTasks.rescheduleTask(task);
+
+        assertEquals(task.getName(), updatedTask.getName());
+        assertEquals(task.getSortOrder(), updatedTask.getSortOrder());
+        assertEquals(task.getIsComplete(), updatedTask.getIsComplete());
+        assertEquals(task.getType(), updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
+        assertEquals(19847, updatedTask.getDueDate());
+        assertEquals(task.getInterval(), updatedTask.getInterval());
+    }
+
+    @Test
+    public void testRescheduleLeapYear() {
+        long leapYearTest = 19782; // February 29th, 2024
+        SuccessoratorTask task = new SuccessoratorTask(null, "Fifth Saturday Task", 0, false, TaskType.Recurring, leapYearTest, leapYearTest, TaskInterval.Yearly);
+        SuccessoratorTask updatedTask = SuccessoratorTasks.rescheduleTask(task);
+
+        assertEquals(task.getName(), updatedTask.getName());
+        assertEquals(task.getSortOrder(), updatedTask.getSortOrder());
+        assertEquals(task.getIsComplete(), updatedTask.getIsComplete());
+        assertEquals(task.getType(), updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
+        assertEquals(20148, updatedTask.getDueDate());
+        assertEquals(task.getInterval(), updatedTask.getInterval());
+
+        // we'll manually check the next four years
+        updatedTask = SuccessoratorTasks.rescheduleTask(updatedTask);
+
+        assertEquals(task.getName(), updatedTask.getName());
+        assertEquals(task.getSortOrder(), updatedTask.getSortOrder());
+        assertEquals(task.getIsComplete(), updatedTask.getIsComplete());
+        assertEquals(task.getType(), updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
+        assertEquals(20513, updatedTask.getDueDate());
+        assertEquals(task.getInterval(), updatedTask.getInterval());
+
+
+        updatedTask = SuccessoratorTasks.rescheduleTask(updatedTask);
+
+        assertEquals(task.getName(), updatedTask.getName());
+        assertEquals(task.getSortOrder(), updatedTask.getSortOrder());
+        assertEquals(task.getIsComplete(), updatedTask.getIsComplete());
+        assertEquals(task.getType(), updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
+        assertEquals(20878, updatedTask.getDueDate());
+        assertEquals(task.getInterval(), updatedTask.getInterval());
+
+
+        updatedTask = SuccessoratorTasks.rescheduleTask(updatedTask);
+
+        assertEquals(task.getName(), updatedTask.getName());
+        assertEquals(task.getSortOrder(), updatedTask.getSortOrder());
+        assertEquals(task.getIsComplete(), updatedTask.getIsComplete());
+        assertEquals(task.getType(), updatedTask.getType());
+        assertEquals(task.getCreateDate(), updatedTask.getCreateDate());
+        assertEquals(21243, updatedTask.getDueDate());
+        assertEquals(task.getInterval(), updatedTask.getInterval());
     }
 }
