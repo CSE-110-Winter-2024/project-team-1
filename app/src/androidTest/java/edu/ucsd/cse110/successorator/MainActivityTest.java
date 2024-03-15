ff7548c04631762c671bdfb28f9eaf36aa3f4266
yeeeee1015 <eric.ye04@yahoo.com>
2024-03-14 19:25:56 -0700

    started ui test framework

--- app/src/androidTest/java/edu/ucsd/cse110/successorator/MainActivityTest.java
+++ app/src/androidTest/java/edu/ucsd/cse110/successorator/MainActivityTest.java
@@ -1,15 +1,15 @@
 package edu.ucsd.cse110.successorator;
 
+import android.view.View;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import org.junit.Test;
 
 import edu.ucsd.cse110.successorator.ui.tasklist.SuccessoratorTaskListFragment;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,5 +17,25 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class MainActivityTest {
+    @Test
+    public void testTaskListFragmentIsDisplayed() {
+        Espresso.onView(withId(R.id.fragment_container))
+                .check(matches(ViewMatchers.isDisplayed()));
+        Espresso.onView(withId(R.id.task_list))
+                .check(matches(ViewMatchers.isDisplayed()));
+    }
 
+    @Test
+    public void testAddTaskFragmentIsDisplayedAfterButtonClick() {
+        Espresso.onView(withId(R.id.add_task_button))
+                .perform(ViewActions.click());
+        // ensure create task fragment appears
+    }
+
+    @Test
+    public void testSwitchContextFragmentIsDisplayedAfterButtonClick() {
+        Espresso.onView(withId(R.id.hamburger_menu_icon))
+                .perform(ViewActions.click());
+        // ensure switch context fragment appears
+    }
 }
