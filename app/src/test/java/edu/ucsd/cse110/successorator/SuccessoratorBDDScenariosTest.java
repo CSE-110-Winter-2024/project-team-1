be97383f3555c6c8396dfe5ddd9c209e2c69be9c
jensenmckenzie <jensen.mckenzie@outlook.com>
2024-03-14 22:08:32 -0700

    created test file

---
+++ app/src/test/java/edu/ucsd/cse110/successorator/SuccessoratorBDDScenariosTest.java
@@ -0,0 +1,15 @@
+package edu.ucsd.cse110.successorator;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class SuccessoratorBDDScenariosTest {
+}
